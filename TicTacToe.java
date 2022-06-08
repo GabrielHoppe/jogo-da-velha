76d94ec2d06440142923d1ce1a467795c9c0732f
Marco Mangan <marco.mangan@pucrs.br>
2022-06-08 10:16:50 -0300

    Insert some JavaDoc elements.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,61 +1,87 @@
 import java.util.Scanner;
 
+/**
+ * A simple Tic-Tac-Toe game.
+ * 
+ * @author marco.mangan@pucrs.br
+ * @version 2022-06-08
+ */
 class TicTacToe {
-  public static String[][] init() {
-    String[][] b = new String[3][3];
-    for (int i = 0; i < b.length; i++) {
-      for (int j = 0; j < b[i].length; j++) {
-        b[i][j] = " ";
-      }
-    }
-    return b;
-  }
-  
-  public static String display(String[][] b) {
-    String r = "";
-    System.out.println("  012");
-    for (int i = 0; i < b.length; i++) {
-      r += i + "|";
-      for (int j = 0; j < b[i].length; j++) {
-        r += b[i][j];
-      }
-      r += "|\n";
-    }
-    return r;
-  }
 
-  public static void set(String[][] b, int i, int j, String p) {
-    if (b[i][j].equals(" ")) {
-      b[i][j] = p;
+    /**
+     * Initialize a new board.
+     * 
+     * @return a 3x3 board
+     */
+    public static String[][] init() {
+        String[][] b = new String[3][3];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                b[i][j] = " ";
+            }
+        }
+        return b;
     }
-  }
-  
-  public static void main(String[] args) {
-    Scanner sc;
-    String[][] board;
-    String player = "x";
-    int i, j;
-    
-    System.out.println("Jogo da Velha!");
-    board = init();
-    sc = new Scanner(System.in);
-    while (true) {
-      System.out.println(display(board));
-      i = sc.nextInt();
-      if (i < 0)
-        break;
-      j = sc.nextInt();
-      
-      set(board, i, j, player);
 
-      if (player.equals("x")) {
-        player = "o";
-      } else {
-        player = "x";
-      }
-      
+    /**
+     * Returns a string with a board representation.
+     * 
+     * @param board a 3x3 board
+     */
+    public static String display(String[][] board) {
+        String r = "";
+        System.out.println("  012");
+        for (int i = 0; i < board.length; i++) {
+            r += i + "|";
+            for (int j = 0; j < board[i].length; j++) {
+                r += board[i][j];
+            }
+            r += "|\n";
+        }
+        return r;
     }
-    sc.close();
-    
-  }
+
+    /**
+     * Set a 
+     * 
+     * @param
+     * @param
+     * @param
+     * @param
+     */
+    public static void set(String[][] b, int i, int j, String p) {
+        if (b[i][j].equals(" ")) {
+            b[i][j] = p;
+        }
+    }
+
+    /**
+     * @param
+     */
+    public static void main(String[] args) {
+        Scanner sc;
+        String[][] board;
+        String player = "x";
+        int i, j;
+
+        System.out.println("Jogo da Velha!");
+        board = init();
+        sc = new Scanner(System.in);
+        while (true) {
+            System.out.println(display(board));
+            i = sc.nextInt();
+            if (i < 0)
+                break;
+            j = sc.nextInt();
+
+            set(board, i, j, player);
+
+            if (player.equals("x")) {
+                player = "o";
+            } else {
+                player = "x";
+            }
+        }
+        sc.close();
+    }
 }
