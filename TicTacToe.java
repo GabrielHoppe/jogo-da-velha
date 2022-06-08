f85d5b1ba0cc7276cf3eff317127735f56b68ff7
Marco Mangan <marco.mangan@pucrs.br>
2022-06-08 10:00:17 -0300

    Code from last class. Class name changed.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,33 +1,61 @@
+import java.util.Scanner;
 
-/**
- * Write a description of class TicTacToe here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class TicTacToe
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class TicTacToe
-     */
-    public TicTacToe()
-    {
-        // initialise instance variables
-        x = 0;
+class TicTacToe {
+  public static String[][] init() {
+    String[][] b = new String[3][3];
+    for (int i = 0; i < b.length; i++) {
+      for (int j = 0; j < b[i].length; j++) {
+        b[i][j] = " ";
+      }
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    return b;
+  }
+  
+  public static String display(String[][] b) {
+    String r = "";
+    System.out.println("  012");
+    for (int i = 0; i < b.length; i++) {
+      r += i + "|";
+      for (int j = 0; j < b[i].length; j++) {
+        r += b[i][j];
+      }
+      r += "|\n";
     }
+    return r;
+  }
+
+  public static void set(String[][] b, int i, int j, String p) {
+    if (b[i][j].equals(" ")) {
+      b[i][j] = p;
+    }
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc;
+    String[][] board;
+    String player = "x";
+    int i, j;
+    
+    System.out.println("Jogo da Velha!");
+    board = init();
+    sc = new Scanner(System.in);
+    while (true) {
+      System.out.println(display(board));
+      i = sc.nextInt();
+      if (i < 0)
+        break;
+      j = sc.nextInt();
+      
+      set(board, i, j, player);
+
+      if (player.equals("x")) {
+        player = "o";
+      } else {
+        player = "x";
+      }
+      
+    }
+    sc.close();
+    
+  }
 }
